95a7a6f8ff52357badfbb79daed7c6ec5e604f93
Benjamin Herron <ben33herron85@gmail.com>
2026-06-13 14:45:52 -0400

    feat: deploy high-fidelity matrix to vercel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'map_screen.dart';
 import 'chat_screen.dart';
 import 'profile_screen.dart';
 import 'shop_screen.dart';
--- lib/screens/shop_screen.dart
+++ lib/screens/shop_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
-import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
