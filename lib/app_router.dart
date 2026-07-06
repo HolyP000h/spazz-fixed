45b980314b76a615c42bd2204b8f407a4609ee6c
Ben <ben33herron85@gmail.com>
2026-07-05 20:58:59 -0400

    app_router: Change initial route to login

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -2,10 +2,9 @@ import 'package:go_router/go_router.dart';
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/map_screen.dart';
-import 'screens/base44_screen.dart'; // 👈 1. ADD THIS IMPORT
 
 final appRouter = GoRouter(
-  initialLocation: '/design',
+  initialLocation: '/login', // <-- Changed from '/design' to '/login'
   routes: [
     GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
     GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
